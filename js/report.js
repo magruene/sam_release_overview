e6009e6f2034ef93b6081cc4c010c22778e2a2c1
Marc Gruenewald (taagrma3) <marc.gruenewald1@swisscom.com>
2015-11-17 08:22:47 +0100

    fix x-editable maybe

--- js/report.js
+++ js/report.js
@@ -82,11 +82,13 @@ function prepareTableRow(team, epic, sumForEpic) {
         type: 'text',
         pk: jQuery(tableBody.find("tr")).length,
         url: "http://jira.swisscom.com/rest/api/2/issue/" + epic.key,
+        params: function (params) {
+            return JSON.stringify("{{'fields':{'customfield_17650':'" + params.value + "'}}}")
+        },
         ajaxOptions: {
             type: "PUT",
             contentType: 'application/json',
-            dataType: "json",
-            data: JSON.stringify("{{'fields':{'customfield_17650':'" + jQuery('#report_detail_' + epic.key).val() + "'}}}")
+            dataType: "json"
         },
         title: 'Update report detail:'
     });
